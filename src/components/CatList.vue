f124c95076cb38264d80cd24dedc516d98d653fc
Jaws <jawsnotfound1@gmail.com>
2019-02-26 22:51:54 +0200

    Added search

--- src/components/CatList.vue
+++ src/components/CatList.vue
@@ -2,7 +2,16 @@
   <div>
     <h1 v-if="!breeds">No breeds to show. This is possibly a problem with the API</h1>
     <div v-if="breeds">
-      <h1 class="catlist_h1">Cat breeds:</h1>
+      <div class="top_bar">
+        <h1 class="catlist_h1">Cat breeds:</h1>
+        <input
+          type="text"
+          id="search_input"
+          v-on:keyup="search($event)"
+          placeholder="Search for breeds..."
+          title="Type in something"
+        >
+      </div>
       <div class="container_catlist">
         <div v-bind:key="breed.name" v-for="breed in breeds" class="container_catlistitem">
           <a
@@ -24,8 +33,24 @@
 </template>
 
 <style lang="scss">
-  .catlist_h1 {
+  .top_bar {
     padding-left: 10px;
+  }
+
+  #search_input {
+    max-height: 30px;
+    box-sizing: border-box;
+    border: 2px solid #ccc;
+    border-radius: 10px;
+    font-size: 16px;
+    background-color: white;
+    background-image: url('https://www.w3schools.com/css/searchicon.png');
+    background-position: 5px 3px;
+    background-repeat: no-repeat;
+    padding: 12px 20px 12px 40px;
+    margin-bottom: 15px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
   }
 
   .container_catlist {
@@ -56,6 +81,10 @@
     color: lightgrey;
     font-size: 14px;
   }
+
+  .hidden {
+    display: none;
+  }
 </style>
 
 <script lang="ts">
@@ -64,5 +93,22 @@
   export default {
     name: 'CatList',
     props: ['breeds'],
+    methods: {
+      search(e: Event) {
+        const searchVal = (<HTMLInputElement>e.target).value;
+        const elems = document.getElementsByClassName('container_catlistitem');
+
+        for (let element of elems) {
+          if (element.firstElementChild) {
+            const breedName = element.firstElementChild.innerHTML.toLowerCase();
+            if (!breedName.includes(searchVal)) {
+              element.className += ' hidden';
+            } else {
+              element.classList.remove('hidden');
+            }
+          }
+        }
+      },
+    },
   };
 </script>
